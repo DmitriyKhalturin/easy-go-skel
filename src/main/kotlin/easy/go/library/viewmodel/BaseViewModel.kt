1fa6839ec18f8eb46f80348dd932f20a13fadcc7
Daniil Zakharov <my@area.gq>
2020-09-16 18:53:28 +0300

    add 'addDisposable' method for single disposable object to use with .also

--- src/main/kotlin/easy/go/library/viewmodel/BaseViewModel.kt
+++ src/main/kotlin/easy/go/library/viewmodel/BaseViewModel.kt
@@ -12,6 +12,8 @@ open class BaseViewModel : ViewModel() {
 
   private val disposables = CompositeDisposable()
 
+  fun addDisposable(disposable: Disposable) = disposables.add(disposable)
+
   fun addDisposable(vararg disposable: Disposable) = disposables.addAll(*disposable)
 
   override fun onCleared() {
