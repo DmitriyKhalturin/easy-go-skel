3f66fd7821b88210a0faa0c68312f0400761b4ca
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2020-10-10 15:22:50 +0300

    Add extension functions.

---
+++ src/main/kotlin/easy/go/library/extention/Function.kt
@@ -0,0 +1,30 @@
+package easy.go.library.extention
+
+/**
+ * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
+ * for babyJa-android on 10.10.20 15:07.
+ */
+
+/**
+ * Get sub list from list without IndexOutOfBoundsException
+ */
+fun <T> List<T>.sublist(offset: Int, limit: Int): List<T> {
+  return when {
+    (offset >= size) -> emptyList()
+    else -> {
+      val toIndex = if (offset + limit < size) {
+        (offset + limit)
+      } else {
+        size
+      }
+
+      subList(offset, toIndex)
+    }
+  }
+}
+
+inline fun <reified T: Any, R: Any> letElements(vararg elements: T?, closure: (Array<T>) -> R): R? {
+  return if (elements.all { it != null }) {
+    closure(elements.filterNotNull().toTypedArray())
+  } else null
+}
