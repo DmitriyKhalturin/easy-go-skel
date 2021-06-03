442accb52cf85ed8e609188d4fdfe9c245ab1f10
Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
2021-06-04 01:34:09 +0300

    Add coroutine libs, replace base classes.

--- src/main/kotlin/easy/go/skel/viewmodel/BaseViewModel.kt
+++ src/main/kotlin/easy/go/skel/viewmodel/BaseViewModel.kt
@@ -1,24 +1,34 @@
 package easy.go.skel.viewmodel
 
 import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
 
 /**
  * Created by Dmitriy Khalturin <dmitry.halturin.86@gmail.com>
  * for easy_go_skel on 03.04.20 0:55.
  */
-open class BaseViewModel : ViewModel() {
+abstract class BaseViewModel : ViewModel() {
 
-  private val disposables = CompositeDisposable()
+  private val supervisor = SupervisorJob()
 
-  fun addDisposable(disposable: Disposable) = disposables.add(disposable)
-
-  fun addDisposable(vararg disposable: Disposable) = disposables.addAll(*disposable)
-
-  override fun onCleared() {
-    disposables.clear()
-
-    super.onCleared()
+  private val exceptionHandler = CoroutineExceptionHandler { _, e ->
+    onExceptionHandler(e)
   }
+
+  abstract fun onExceptionHandler(e: Throwable)
+
+  protected val coroutineContext = (supervisor + exceptionHandler)
+
+  protected inline fun launch(crossinline block: suspend CoroutineScope.() -> Unit) =
+    viewModelScope.launch(coroutineContext) {
+      block()
+    }
+
+  protected inline fun <T> async(crossinline block: suspend CoroutineScope.() -> T): Deferred<T> =
+    viewModelScope.async(coroutineContext) {
+      block()
+    }
+
+  // TODO: implement later: produce, actor, broadcast
 }
